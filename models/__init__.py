fc5a8dc77d9ac5c211281dbd2866f64727133ea7
qxde01 <qxde01@gmail.com>
2019-11-09 14:45:26 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,8 @@ from .mobilenet_v2 import MobileNetV2
 from .mobilenet import MobileNet
 from .DenseShuffle import DenseShuffleV2,DenseShuffleV1,DenseNet
 from .mobilenet_v3 import MobileNetV3Large,MobileNetV3Small
-from .resnet import ResNet18,ResNet34,ResNet18V2,ResNet34V2
+from .resnet import ResNet18,ResNet34,ResNet18V2,ResNet34V2,ResNet50,ResNet50V2,ResNet101,ResNet101V2,ResNet152,ResNet152V2,ResNeXt50,ResNeXt101
 from .squeezenet import SqueezeNet
 from .nasnet import NASNetMobile,NASNetLarge
 from .mixnets import MixNetMedium,MixNetLarge,MixNetSmall
+from .residual_attention_network import AttentionResNet56,AttentionResNet92
