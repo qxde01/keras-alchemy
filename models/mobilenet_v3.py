7bd286e6988fceda6e17cf119609c9800251ec43
qxde01 <qxde01@gmail.com>
2020-01-05 21:52:32 +0800

    Update mobilenet_v3.py

--- models/mobilenet_v3.py
+++ models/mobilenet_v3.py
@@ -16,8 +16,8 @@ def Hswish(x):
 keras.utils.get_custom_objects().update({'custom_activation': keras.layers.Activation(Hswish)})
 
 def __conv2d_block(_inputs, filters, kernel, strides, is_use_bias=False, padding='same', activation='RE', name=None):
-    x = keras.layers.Conv2D(filters, kernel, strides= strides, padding=padding, use_bias=is_use_bias)(_inputs)
-    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Conv2D(filters, kernel, strides= strides, padding=padding, use_bias=is_use_bias,kernel_initializer='he_normal')(_inputs)
+    x = keras.layers.BatchNormalization(momentum=0.9,epsilon=1e-5)(x)
     if activation == 'RE':
         x = keras.layers.ReLU(name=name)(x)
     elif activation == 'HS':
@@ -27,8 +27,8 @@ def __conv2d_block(_inputs, filters, kernel, strides, is_use_bias=False, padding
     return x
 
 def __depthwise_block(_inputs, kernel=(3, 3), strides=(1, 1), activation='RE', is_use_se=True, num_layers=0):
-    x = keras.layers.DepthwiseConv2D(kernel_size=kernel, strides=strides, depth_multiplier=1, padding='same')(_inputs)
-    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.DepthwiseConv2D(kernel_size=kernel, strides=strides, depth_multiplier=1, padding='same',kernel_initializer='he_normal')(_inputs)
+    x = keras.layers.BatchNormalization(momentum=0.9,epsilon=1e-5)(x)
     if is_use_se:
         x = __se_block(x)
     if activation == 'RE':
@@ -45,7 +45,7 @@ def __global_depthwise_block(_inputs):
     assert _inputs.shape[1] == _inputs.shape[2]
     #kernel_size = _inputs._keras_shape[1]
     kernel_size =int( _inputs.shape[1])
-    x = keras.layers.DepthwiseConv2D((kernel_size, kernel_size), strides=(1, 1), depth_multiplier=1, padding='valid')(_inputs)
+    x = keras.layers.DepthwiseConv2D((kernel_size, kernel_size), strides=(1, 1), depth_multiplier=1, padding='valid',kernel_initializer='he_normal')(_inputs)
     return x
 
 def __se_block(_inputs, ratio=4, pooling_type='avg'):
@@ -77,13 +77,13 @@ def __bottleneck_block(_inputs, out_dim, kernel, strides, expansion_dim, is_use_
         x = __depthwise_block(x, kernel=kernel, strides=strides, is_use_se=is_use_se, activation=activation, num_layers=num_layers)
 
         # ** pointwise conv
-        x = keras.layers.Conv2D(out_dim, (1, 1), strides=(1, 1), padding='same')(x)
-        x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.Conv2D(out_dim, (1, 1), strides=(1, 1), padding='same',kernel_initializer='he_normal')(x)
+        x = keras.layers.BatchNormalization(momentum=0.9,epsilon=1e-5)(x)
 
         if shortcut and strides == (1, 1):
             in_dim = K.int_shape(_inputs)[-1]
             if in_dim != out_dim:
-                ins = keras.layers.Conv2D(out_dim, (1, 1), strides=(1, 1), padding='same')(_inputs)
+                ins = keras.layers.Conv2D(out_dim, (1, 1), strides=(1, 1), padding='same',kernel_initializer='he_normal')(_inputs)
                 x = keras.layers.Add()([x, ins])
             else:
                 x = keras.layers.Add()([x, _inputs])
@@ -94,7 +94,8 @@ def build_mobilenet_v3(input_shape=(224,224,3), num_classes=1000, model_type='la
     inputs = keras.layers.Input(shape=input_shape)
 
     # ** feature extraction layers
-    net = __conv2d_block(inputs, 16, kernel=(3, 3), strides=(2, 2), is_use_bias=False, padding='same', activation='HS') 
+    # cifar100 strides=(2, 2)=>(1,1)
+    net = __conv2d_block(inputs, 16, kernel=(3, 3), strides=(1, 1), is_use_bias=False, padding='same', activation='HS')
 
     if model_type == 'large':
         config_list = large_config_list
@@ -109,6 +110,12 @@ def build_mobilenet_v3(input_shape=(224,224,3), num_classes=1000, model_type='la
     # ** final layers
     net = __conv2d_block(net, 960, kernel=(3, 3), strides=(1, 1), is_use_bias=True, padding='same', activation='HS', name='output_map')
 
+    net = keras.layers.Conv2D(1280, (1, 1), strides=(1, 1), padding='valid', use_bias=True,
+                              kernel_initializer='he_normal')(net)
+
+    net = keras.layers.BatchNormalization(momentum=0.9, epsilon=1e-5)(net)
+    net = keras.layers.Activation(Hswish)(net)
+
     if pooling_type == 'avg':
         net = keras.layers.GlobalAveragePooling2D()(net)
     elif pooling_type == 'depthwise':
@@ -117,15 +124,19 @@ def build_mobilenet_v3(input_shape=(224,224,3), num_classes=1000, model_type='la
         raise NotImplementedError
 
     # ** shape=(None, channel) --> shape(1, 1, channel) 
-    pooled_shape = (1, 1, int(net.shape[-1]))
+    #pooled_shape = (1, 1, int(net.shape[-1]))
 
-    net = keras.layers.Reshape(pooled_shape)(net)
-    net = keras.layers.Conv2D(1280, (1, 1), strides=(1, 1), padding='valid', use_bias=True)(net)
+    #net = keras.layers.Reshape(pooled_shape)(net)
+    #net = keras.layers.Conv2D(1280, (1, 1), strides=(1, 1), padding='valid', use_bias=True,kernel_initializer='he_normal')(net)
+
+    #net=keras.layers.BatchNormalization(momentum=0.9,epsilon=1e-5)(net)
+    #net = keras.layers.Activation(Hswish)(net)
     
     if include_top:
-        net = keras.layers.Conv2D(num_classes, (1, 1), strides=(1, 1), padding='valid', use_bias=True)(net)
-        net = keras.layers.Flatten()(net)
-        net = keras.layers.Softmax()(net)
+        net=keras.layers.Dense(num_classes,activation='softmax')(net)
+        #net = keras.layers.Conv2D(num_classes, (1, 1), strides=(1, 1), padding='valid', use_bias=True,kernel_initializer='he_normal')(net)
+        #net = keras.layers.Flatten()(net)
+        #net = keras.layers.Softmax()(net)
 
     model = keras.models.Model(inputs=inputs, outputs=net)
 
@@ -138,7 +149,7 @@ global large_config_list
 global small_config_list
 
 large_config_list = [[16,  (3, 3), (1, 1), 16,  False, False, False, 'RE',  0],
-                     [24,  (3, 3), (2, 2), 64,  False, False, False, 'RE',  1],
+                     [24,  (3, 3), (1, 1), 64,  False, False, False, 'RE',  1],#strides=(2, 2)=>(1,1)
                      [24,  (3, 3), (1, 1), 72,  False, True,  False, 'RE',  2],
                      [40,  (5, 5), (2, 2), 72,  False, False, True,  'RE',  3],
                      [40,  (5, 5), (1, 1), 120, False, True,  True,  'RE',  4],
@@ -173,7 +184,9 @@ def MobileNetV3Small(include_top=True,input_shape=(224,224,3), num_classes=10, p
 
 """ build MobileNet V3 model """
 if __name__ == '__main__':
-    model = MobileNetV3Large(include_top=True,input_shape=(416,416,3), num_classes=10, pooling='avg')
+    # MobileNetV3Large,5,246,972     5,252,092
+    # MobileNetV3Small,3,068,796    3,073,916
+    model = MobileNetV3Large(include_top=True,input_shape=(32,32,3), num_classes=100, pooling='avg')
     print(model.summary())
     keras.utils.plot_model(model, 'MobileNetV3Large.png', show_shapes=True)
     #print(model.layers)
